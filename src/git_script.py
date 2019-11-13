2f76d3940d98ed186b1a58a954f04e0da40bec43
1garo <alevardai@hotmail.com>
2019-11-12 23:04:21 -0200

    script seems to be working fine

--- src/git_script.py
+++ src/git_script.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 
+
+# hash do commit antigo a410a66cd4da793803c707d3beb3cee41e30ea8c...
+
+
+import os
 import subprocess
-import sys, os
+import sys
+from os import path
+
+from subprocess import Popen, PIPE
+hash_value = []
 
 
-def main():
+def create():
     commit_hash = get_last_commit_hash()
-    msg = os.system("git diff-tree --no-commit-id --name-only -r " + str(commit_hash))
-    print(msg)
+    msg = """
+    git show --pretty=\"format:\" --name-only {commit_hash}\""
+    """.format(commit_hash=commit_hash)
+    sys_msg = os.system(msg)
+    # commit_commands = [msg, "status"]
+    # pwd = os.system("pwd")
+
+    return sys_msg
 
 
 def get_last_commit_hash():
     """Get the hash of the last commited commit"""
-    return os.system("git log -n 1 HEAD --pretty=format:\"%H\"")
+    msg = os.popen("git log -n 1 master --pretty=format:\"%H\"").read()
+
+    return msg
 
 
 if __name__ == '__main__':
-    main()
+    create()
