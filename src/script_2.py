d68c05f3034c877530531ebe98503062e10278c0
alevardai <alevardai@hotmail.com>
2019-11-14 13:30:31 -0300

    test

--- src/script_2.py
+++ src/script_2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 
-def main(param):
-    for i in range(len(param)):
-        aux = param[i]
-        name = aux.split("/", 1)[0]
-
-        if name == "XXX":
-            return 1
-        else:
-            return 0
+def main(files):
+    div = files.split("/")
+    svc_name = div[0]
+    if len(div) == 1:
+        print(svc_name + " : Arquivo na raiz do projeto.")
+        return 1
+    if len(div[-1]) > 500:
+        print("Erro no servico:" + div[0] + ": Nome muito longo")
+        return 1
+    return 0
